a692d9e3fcf489f6beaf8ae5367333939332524d
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-09 12:37:55 +0100

    Clase GraphNode Comentada

--- Grafo/GraphNode.h
+++ Grafo/GraphNode.h
@@ -7,7 +7,7 @@ using namespace std;
 class GraphNode {
     public:      
         int id;
-        vector<int> vecinos;
+        vector<int> vecinos; //Lista de id de los vecinos del nodo
 
 
         GraphNode(int _id){
@@ -18,19 +18,19 @@ class GraphNode {
             vecinos = _vecinos;
         }
     
-        bool tieneVecino(int id) const {
+        bool tieneVecino(int id) const { //Comprueba si el nodo tiene vecinos
             for (auto it : vecinos)
                 if (it == id) return true;
             return false;
         }
 
-        void annyairVecino(int vid){
+        void annyairVecino(int vid){ //Añade un vecino a la lista dada su id, siempre que no exista ya
             if (!tieneVecino(vid)) //comprueba si el vecino ya existe
                 vecinos.push_back(vid);
             else throw std::runtime_error("Veicno ya es vecino del nodo");
         }
-        void eliminarVecino(int id){
-        for (size_t i = 0; i < vecinos.size(); ++i) {
+        void eliminarVecino(int id){ //Elimina un vecino de la lista dada su id
+        for (int i = 0; i < vecinos.size(); ++i) {
             if (vecinos[i] == id) {
                 vecinos.erase(vecinos.begin() + i);
                 return;
