b22feabe94595115ade77765c9c510f3e2443cba
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-18 18:15:05 +0100

    Update Reactor_2.cpp

--- Day_11/Reactor_2.cpp
+++ Day_11/Reactor_2.cpp
@@ -8,9 +8,10 @@
 
 //#define DEBUG
 using namespace std;
-HashTable<int> tabla = HashTable<int>(700);
-Graph grafo;
-void load(vector<vector<string>> &out){
+HashTable<int> tabla = HashTable<int>(700); //Tabla hash que almacenará las combinaciones de tres letras y su id
+Graph grafo; //Grafo con el que se calcularán la cantidad de caminos posibles
+
+void load(vector<vector<string>> &out){ //Función que, a partir del input crea un grafo y una tabla hash y guarda así mismo el input en una matriz de strings
     ifstream input("input.txt");
     string str,line;
     int cont=0;
@@ -18,11 +19,11 @@ void load(vector<vector<string>> &out){
         vector<string> v_aux;                               //Creamos un vector auxiliar
         stringstream ss(line);                              //Creamos un string stream para leer la linea
         while(ss>>str){ 
-           if(str.size() > 3){
-            str = str.substr(0,3);
-            grafo.insertNode(cont);
-            tabla.insert(str,cont);
-            cont++;
+           if(str.size() > 3){                             // Si es la primera combinación de la linea ( abc: por ejemplo)
+            str = str.substr(0,3);                         // Se adapta la string (abc: -> abc)
+            grafo.insertNode(cont);                        //Se inserta en el grafo
+            tabla.insert(str,cont);                        //Se inserta en la tabla hash
+            cont++;                                        //Se actualiza cont
            }
             v_aux.push_back(str);
         }               
@@ -33,26 +34,27 @@ void load(vector<vector<string>> &out){
     tabla.insert("out",cont);
 }
 
-void add_neig(const vector<vector<string>> &adj){ //Funcion que une los nodos dada la lista de adyacencia
+void add_neig(const vector<vector<string>> &adj){            //Funcion que une los nodos dada la lista de adyacencia
     for(int i = 0; i<adj.size();i++){
-        int idA = tabla.search(adj[i][0]); //Buscas en la tabla la id del nodo del que se parte
-        for (int j = 1;j<adj[i].size();j++){ // Le añades los vecinos
+        int idA = tabla.search(adj[i][0]);                   //Buscas en la tabla la id del nodo del que se parte
+        for (int j = 1;j<adj[i].size();j++){                 // Le añades los vecinos
             int idB = tabla.search(adj[i][j]); 
-            grafo.addEdge(idA,idB,true); // Unes Nodo A con Nodo B de forma dirigida
+            grafo.addEdge(idA,idB,true);                     // Unes Nodo A con Nodo B de forma dirigida
         }
     }
 }
 
-long long path_count(int id,vector<vector<vector<long long>>> &mem,bool fft, bool dac){
-    if( mem[id][fft][dac] != -1) return mem[id][fft][dac];
-    if (id == tabla.search("fft")) fft = true;
-    if(id == tabla.search("dac")) dac = true;
-    if (id == tabla.search("out")) { if ( fft && dac )return 1; else return 0;}
+long long path_count(int id,vector<vector<vector<long long>>> &mem,bool fft = false, bool dac = false){
+    
+    if( mem[id][fft][dac] != -1) return mem[id][fft][dac];    //Si la respuesta ya existe devuelves directamente la respuesta
+    if (id == tabla.search("fft")) fft = true;                //Si pasas por el nodo fft lo "activas"
+    if(id == tabla.search("dac")) dac = true;                 //Si pasas por el nodo dac lo "activas"
+    if (id == tabla.search("out")) { if ( fft && dac )return 1; else return 0;} //Si llegas al nodo out compruebas si has pasado por ambos nodos, si has pasado devuelves 1 sino 0
     int cantidad_vecinos = grafo.graph[id].cantidad_vecinos();
     long long sum = 0;
-    for(int i=0; i < cantidad_vecinos;i++){
+    for(int i=0; i < cantidad_vecinos;i++){                    //Expandes para todos los vecinos del nodo en el que estas
         int new_id = grafo.graph[id].vecinos[i];
-        sum += path_count(new_id,mem,fft,dac);
+        sum += path_count(new_id,mem,fft,dac);                //Guardas el resultado en un sumatorio
     }
     mem[id][fft][dac] = sum;
     return mem[id][fft][dac];
@@ -68,10 +70,9 @@ int main(){
     grafo.print(); cout<<endl;
 #endif
     //Búsqueda de los nodos inicio y final
-    int id_svr = tabla.search("svr"); 
-    bool fft = false,dac = false;
-    //aplicación de algoritmo para saber cuantos caminos hay de un nodo a otro
-    vector<vector<vector<long long>>> mem(grafo.graph.size(),vector<vector<long long>>(2,vector<long long>(2,-1)));
-    cout<<path_count(id_svr,mem,fft,dac)<<endl;
+    int id_svr = tabla.search("svr"); //Id desde la cual se parte
+
+    vector<vector<vector<long long>>> mem(grafo.graph.size(),vector<vector<long long>>(2,vector<long long>(2,-1))); //matriz de memoria de tamaño,  [input x 2 x 2]
+    cout<<path_count(id_svr,mem)<<endl;
     
 }
