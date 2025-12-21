6514ff34b40d82596d35ca14fa2d827f0a39dae1
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-21 20:04:57 +0100

    Update Cafeteria.cpp

--- Day_5/Cafeteria.cpp
+++ Day_5/Cafeteria.cpp
@@ -17,20 +17,21 @@ int main() {
         return 1;
     }
 
-    BSTree<Interval> tree;
+    BSTree<Interval> tree; //Arbol de intervalos
     string line;
-    vector<Interval> inter;
+    vector<Interval> inter; //Vector de invervalos
     int frescos = 0;
-    // Leer rangos
+    
+
     while (getline(input, line)) {
-        if (line.empty()) {
-            sort(inter.begin(), inter.end());
+        if (line.empty()) { // En la linea vacía
+            sort(inter.begin(), inter.end()); //Ordenamos el vector
             int mid = inter.size() /2;
         #ifdef DEBUG
             for (int i = 0; i< inter.size();i++) cout<<inter[i]<<" ";
             cout<<endl;
         #endif
-            for (int i = mid,j=mid-1; j > 0 , i<inter.size(); i++ , j-- ){
+            for (int i = mid,j=mid-1; j > 0 , i<inter.size(); i++ , j-- ){ //Insertamos en el árbol de forma que no quede muy desbalanceado
                 tree.insert(inter[i]);
                 tree.insert(inter[j]);
             }
@@ -41,12 +42,12 @@ int main() {
         else 
         {
             int dash = line.find('-');
-            if (dash != -1){
+            if (dash != -1){ //si contiene -
                 long long a = stoll(line.substr(0, dash));
                 long long b = stoll(line.substr(dash + 1));
                 inter.push_back({a,b});
             }
-            else{
+            else{ //Si no contiene -
                 long long id = stoll(line);
                 if (tree.searchRange(id))
                     frescos++;
@@ -59,3 +60,4 @@ int main() {
     cout << frescos << endl;
     return 0;
 }
+
