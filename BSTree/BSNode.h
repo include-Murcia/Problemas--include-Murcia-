be3decd19ef40e85edb92106106e1fdd7ebb0a53
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-21 20:07:10 +0100

    Update BSNode.h

--- BSTree/BSNode.h
+++ BSTree/BSNode.h
@@ -10,11 +10,13 @@ class BSNode{
     T elemento;
     BSNode<T>* left;
     BSNode<T>* right;
-    long long maxSubtree;
+
+    long long maxSubtree; //Elemento máximo del subarbor generado por el nodo, solo es útil en un árbol de intervalos
 
 
     BSNode(const T& e): elemento(e),left(nullptr),right(nullptr),maxSubtree(e.max) {} 
 
 };
+
 
 #endif
